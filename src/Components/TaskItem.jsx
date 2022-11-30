6e4f3ce15aff0429a21967a8dd07724e56156940
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2022-11-30 15:14:30 -0300

    feat: add description and isCompleted in application

--- src/Components/TaskItem.jsx
+++ src/Components/TaskItem.jsx
@@ -1,3 +1,12 @@
-const TaskItem = () => {}
+import Task from './Task'
+
+const TaskItem = ({ task }) => {
+  return (
+    <div>
+      <h1>{task.description}</h1>
+      <p>{task.isCompleted ? 'Completa' : 'Não completa'}</p>
+    </div>
+  )
+}
 
 export default TaskItem
