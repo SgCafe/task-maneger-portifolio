def10a3b85b864b3c1d4d159c04f0c7fd6a0acc8
Sérgio Gabriel <sgabriel.cafe@gmail.com>
2022-11-30 15:17:12 -0300

    feat: add connection to api and task, and add structure to website

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,26 +1,23 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+import TaskItem from './TaskItem'
+
+import './Task.scss'
 
 const Task = () => {
-  const [task, setTasks] = useState([
-    {
-      id: '1',
-      description: 'Ir na acad',
-      isCompleted: true,
-    },
-    {
-        id: '2',
-        description: 'prog',
-        isCompleted: false,
-      },
-  ])
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    fetchTasks()
+  })
 
   const fetchTasks = async () => {
     try {
-      const response = axios.get(
+      const { data } = await axios.get(
         'https://fsc-task-manager-backend.herokuapp.com/tasks',
       )
-      console.log(response)
+      setTasks(data)
     } catch (error) {
       console.log(error)
     }
@@ -28,8 +25,40 @@ const Task = () => {
 
   return (
     <>
-      <div>
-        {task.map((tasks) =>)}
+      <div className="application">
+        <h1>Gerenciador de Tarefas</h1>
+
+        <div className="panel">
+          <div className="task-container">
+            <h2>Minhas tarefas</h2>
+            <div className="box-task-noCompleted">
+              <div className="tasks-list">
+                {tasks
+                  .filter((task) => task.isCompleted === false)
+                  .map((lastTasks) => (
+                    <p>{lastTasks.description}</p>
+                  ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="task-container">
+            <h2>Tarefas concluídas</h2>
+            <div className="box-task-completed">
+              <div className="tasks-list">
+                {tasks
+                  .filter((task) => task.isCompleted === true)
+                  .map((completedTasks) => (
+                    <p>{completedTasks.description}</p>
+                  ))}
+              </div>
+            </div>
+          </div>
+
+          <div className="pomodoro">
+            <h3>Pomodoro</h3>
+          </div>
+        </div>
       </div>
     </>
   )
